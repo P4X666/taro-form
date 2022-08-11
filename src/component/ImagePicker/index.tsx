8234a902d91a1b813a265e77766879e0a0166c29
shixing.peng <shixing.peng@socialcredits.cn>
2022-08-11 15:58:20 +0800

    取代 uuid

--- src/component/ImagePicker/index.tsx
+++ src/component/ImagePicker/index.tsx
@@ -4,8 +4,40 @@ import React, { ReactNode } from "react";
 import { Image, View } from "@tarojs/components";
 import { ITouchEvent } from "@tarojs/components/types/common";
 import Taro from "@tarojs/taro";
-import { uuid } from "taro-ui/dist/index";
 import { AtImagePickerProps, File } from "taro-ui/types/image-picker";
+
+function uuid(len = 8, radix = 16): string {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split(
+    ""
+  );
+  const value: string[] = [];
+  let i = 0;
+  radix = radix || chars.length;
+
+  if (len) {
+    // Compact form
+    for (i = 0; i < len; i++) value[i] = chars[0 | (Math.random() * radix)];
+  } else {
+    // rfc4122, version 4 form
+    let r;
+
+    // rfc4122 requires these characters
+    /* eslint-disable-next-line */
+    value[8] = value[13] = value[18] = value[23] = "-";
+    value[14] = "4";
+
+    // Fill in random data.  At i==19 set the high bits of clock sequence as
+    // per rfc4122, sec. 4.1.5
+    for (i = 0; i < 36; i++) {
+      if (!value[i]) {
+        r = 0 | (Math.random() * 16);
+        value[i] = chars[i === 19 ? (r & 0x3) | 0x8 : r];
+      }
+    }
+  }
+
+  return value.join("");
+}
 
 interface MatrixFile extends Partial<File> {
   type: "blank" | "btn";
